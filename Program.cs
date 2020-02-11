ae3777c50ae4a7eccb50fd34689b2e49e4d007e0
vasekpekarek <vaclavpekarek@centrum.cz>
2020-02-11 22:11:39 +0100

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatumNarozeni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Narozeniny(new DateTime(1984, 02, 09));
+            Console.ReadKey();
+        }
+        static void Narozeniny(DateTime datumNarozeni)
+        {
+            DateTime dnes = DateTime.Today;
+            int vek = dnes.Year - datumNarozeni.Year;
+            if (dnes < datumNarozeni.AddYears(vek))
+            {
+                vek--;
+                Console.WriteLine(vek);
+            }
+            else
+            {
+                Console.WriteLine(vek);
+            }
+            DateTime dalsiNarozeniny = datumNarozeni.AddYears(vek + 1);
+            TimeSpan rozdil = dalsiNarozeniny - dnes;
+            
+            Console.WriteLine(Convert.ToInt32(rozdil.TotalDays));
+        }
+    }
+}
